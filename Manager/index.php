36780ae6b90b6215ee3eea4493b55692095fd9c6
syatirah <amirsyatirah@gmail.com>
2021-09-19 10:06:40 +0800

    edit projek

---
+++ Manager/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>DFP4064 PROJEK WEB PROGRAMMING</title>
+    </head>
+    <body>
+        <h1>Nama Sistem : Produk Alatan Farmasi (PFA)</h1>
+        <h3>Manager(Syatirah)</h3>
+        <p><a href="index.php">Manager</a></p>
+
+        <h3>Staff(Shoban)</h3>
+        <p><a href="index.php">Staff</a></p>
+    <hr></hr>
+    </body>
+    </html>
