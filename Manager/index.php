1062a6357598163992ae415a0fdfc2dadf371f08
syatirah <amirsyatirah@gmail.com>
2021-09-19 20:39:09 +0800

    try push database

--- Manager/index.php
+++ Manager/index.php
@@ -1,14 +1,87 @@
+<!DOCTYPE html>
 <html>
     <head>
-        <title>DFP4064 PROJEK WEB PROGRAMMING</title>
-    </head>
-    <body>
+    <title>PFA</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+</head>
+<body>
+    <div class="header">
         <h1>Nama Sistem : Produk Alatan Farmasi (PFA)</h1>
-        <h3>Manager(Syatirah)</h3>
+        <h3>PRODUK ALATAN FARMASI</h3>
+        <p>by Optimum</p>
+        </div>
+
+        <nav class="w3-bar w3-black">
+        <a href="index.php" class="w3-button w3-bar-item">Home</a>
+        <a href="pesanan.php" class="w3-button w3-bar-item">PRODUCT</a>
+        <a href="logout.php" class="w3-button w3-bar-item">LOGOUT</a>
+        </nav>
+
+<h3>Manager(Syatirah)</h3>
         <p><a href="syatirah.php">Manager</a></p>
 
         <h3>Staff(Shoban)</h3>
         <p><a href="index.php">Staff</a></p>
-    <hr></hr>
-    </body>
-    </html>
+        <hr></hr>
+ <center>
+<section class="w3-container w3-center" style="max-width:600px">
+  <h2 class="w3-wide">PRODUK ALATAN FARMASI</h2>
+  <p class="w3-opacity"><i>by Optimum</i></p>
+</section>
+
+<table border="1">
+  <tr>
+    <th>Bil</th>
+    <th>PRODUK</th>
+    <th>HARGA</th>
+    <th>JUMLAH</th>
+    <th>STATUS</th>
+    <th>AKSI</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>ARM BLOOD PRESSURE</td>
+    <td>RM350</td>
+    <td>500</td>
+    <td>BELUM BAYAR</td>
+    <td><button><a href="produk.php">BUY</a></button></td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>MINI FISIOTERAPI</td>
+    <td>RM190</td>
+    <td>160</td>
+    <td>BELUM BAYAR</td>
+    <td><button><a href="produk.php">BUY</a></button></td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>GLUCOSE MONITOR SYSTEM</td>
+    <td>RM90</td>
+    <td>400</td>
+    <td>BELUM BAYAR</td>
+    <td><button><a href="produk.php">BUY</a></button></td> 
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>VITAMIN C</td>
+    <td>RM70</td>
+    <td>100</td>
+    <td>BELUM BAYAR</td>
+    <td><button><a href="produk.php">BUY</a></button></td>
+  </tr>
+    <tr>
+    <td>5</td>
+    <td>VITAMIN E</td>
+    <td>RM120</td>
+    <td>50</td>
+    <td>BELUM BAYAR</td>
+    <td><button><a href="produk.php">BUY</a></button></td>
+  </tr>
+
+</table>
+</center>
+</body>
+</html>
